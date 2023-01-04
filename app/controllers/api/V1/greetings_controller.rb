d7a978b7c7c4a82d380f2cf283c5f29232c5dc30
cmolinan <cmolinan@gmail.com>
2023-01-04 15:25:45 -0500

    resolve cors problem and change to API output

--- app/controllers/api/V1/greetings_controller.rb
+++ app/controllers/api/V1/greetings_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    message = Greeting.order(Arel.sql('RANDOM()')).first.message
-    # debugger
-    # render json: { status: 'SUCCESS', message: 'Random message generated', data: message }, status: :ok
+    message = Greeting.order(Arel.sql('RANDOM()')).first
     render json: { message: }
   end
 end
